2e3c39212f99331f83101d3501e42bfbc1aa5d1f
Bedirian <Karni.Bedirian@mountsinai.org>
2025-10-28 16:19:18 -0400

    add plot summary

--- 05_full_span_neuroscore_analysis_mitt.R
+++ 05_full_span_neuroscore_analysis_mitt.R
@@ -11,6 +11,7 @@ tables <- list()
 plots <- list()
 statistics <- list()
 pim_results <- list()
+tmp_summary <- list()
 
 ylabel <- "Full SPAN Score at Day 30"
 
@@ -903,7 +904,7 @@ tmp.pval <- tmp |>
   select(variable, pvalue)
 pvalue_treatment = format_pvalue(tmp.pval$pvalue)
 
-plots$pim$imputation_mi$overall$reperfusion <- 
+plots$pim$imputation_mi$overall$txas_reperfusion <- 
   plot_treat_sig(data = dt,
                  y = full_span_score, 
                  ylabel = ylabel,
@@ -974,6 +975,62 @@ plots$pim$imputation_mi$cl4 <-
            y = full_span_score, 
            ylabel = ylabel,
            p_values = pvalue_treatment)
+
+# Summary ----
+
+## Treatment Overall ----
+
+tmp_summary$tx$overall <- list(
+  noimputation        = tables$pim$noimputation$overall$txas_reperfusion,
+  imputation_ws_overall   = tables$pim$imputation_ws_overall$overall$txas_reperfusion,
+  imputation_ws_cls       = tables$pim$imputation_ws_cls$overall$txas_reperfusion,
+  imputation_ws_clst      = tables$pim$imputation_ws_clst$overall$txas_reperfusion,
+  imputation_mi           = tables$pim$imputation_mi$overall$txas_reperfusion
+)
+
+tmp <- tmp_summary$tx$overall
+
+plots$summary$tx$overall <- plot_summary(tmp, comparison = "treatment")
+
+## Clot Length Overall ----
+
+tmp_summary$clot_length$overall <- list(
+  noimputation        = tables$pim$noimputation$overall$clot_length,
+  imputation_ws_overall   = tables$pim$imputation_ws_overall$overall$clot_length,
+  imputation_ws_cls       = tables$pim$imputation_ws_cls$overall$clot_length,
+  imputation_ws_clst      = tables$pim$imputation_ws_clst$overall$clot_length,
+  imputation_mi           = tables$pim$imputation_mi$overall$clot_length
+)
+
+tmp <- tmp_summary$clot_length$overall
+
+plots$summary$clot_length$overall <- plot_summary(tmp, comparison = "clot_length")
+
+## CL3 ----
+tmp_summary$cl3 <- list(
+  noimputation        = tables$pim$noimputation$cl3$txas_reperfusion,
+  imputation_ws_overall   = tables$pim$imputation_ws_overall$cl3$txas_reperfusion,
+  imputation_ws_cls       = tables$pim$imputation_ws_cls$cl3$txas_reperfusion,
+  imputation_ws_clst      = tables$pim$imputation_ws_clst$cl3$txas_reperfusion,
+  imputation_mi           = tables$pim$imputation_mi$cl3$txas_reperfusion
+)
+
+tmp <- tmp_summary$cl3
+
+plots$summary$cl3 <- plot_summary(tmp, comparison = "treatment")
+
+## CL4 ----
+tmp_summary$cl4 <- list(
+  noimputation        = tables$pim$noimputation$cl4$txas_reperfusion,
+  imputation_ws_overall   = tables$pim$imputation_ws_overall$cl4$txas_reperfusion,
+  imputation_ws_cls       = tables$pim$imputation_ws_cls$cl4$txas_reperfusion,
+  imputation_ws_clst      = tables$pim$imputation_ws_clst$cl4$txas_reperfusion,
+  imputation_mi           = tables$pim$imputation_mi$cl4$txas_reperfusion
+)
+
+tmp <- tmp_summary$cl4 
+
+plots$summary$cl4 <- plot_summary(tmp, comparison = "treatment")
 
 # Saving output ----
 
